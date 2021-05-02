34b2e79b2bcdb39b6ce3b8e2d90378de5cc25281
Inna Dobrynina <a-ya-dobraya@yandex.ru>
2021-05-02 20:20:19 +0500

    imperative popup

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import '../index.css';
+import closeIcon from '../images/close_icon.svg'
 import Header from './Header.js';
 import Main from './Main.js';
 import Footer from './Footer.js';
 
 
 function App() {
+
   return (
     <div className="root">
-      < Header />
-      < Main />
+      <Header />
+      <Main />
       <Footer />
 
 
@@ -21,7 +23,7 @@ function App() {
           <input className="popup__input popup__input_about" type="text" id="about-name" name="about" value="" placeholder="Род деятельности" aria-label="О себе" required minlength="2" maxlength="200" />
           <span className="popup__input-error popup__input-error_about" id="about-name-error"></span>
           <button className="popup__form-submit" type="submit">Сохранить</button>
-          <button className="popup__close" type="button"><img src="<%=require('./images/close_icon.svg')%>" className="popup__close-image" alt="кнопка закрытия" /></button>
+          <button className="popup__close" type="button"><img src={closeIcon} className="popup__close-image" alt="кнопка закрытия" /></button>
         </form>
       </section>
 
@@ -33,13 +35,13 @@ function App() {
           <input className="popup__input popup__input_image" type="url" name="link" id="link-image" value="" placeholder="Ссылка на картинку" required />
           <span className="popup__input-error popup__input-error_image" id="link-image-error"></span>
           <button className="popup__form-submit " type="submit">Создать</button>
-          <button className="popup__close" type="button"><img src="<%=require('./images/close_icon.svg')%>" className="popup__close-image" alt="кнопка закрытия" /></button>
+          <button className="popup__close" type="button"><img src={closeIcon} className="popup__close-image" alt="кнопка закрытия" /></button>
         </form>
       </section>
 
       <section className="popup popup-image">
         <form className="popup-image__container" name="popup-image">
-          <button className="popup__close" type="button"><img src="<%=require('./images/close_icon.svg')%>" className="popup-image__close-btn" alt="кнопка закрытия" /></button>
+          <button className="popup__close" type="button"><img src={closeIcon} className="popup-image__close-btn" alt="кнопка закрытия" /></button>
           <img src="#" alt="#" className="popup-image__content" id="img01" />
           <div className="popup-image__caption"></div>
         </form>
@@ -47,7 +49,7 @@ function App() {
 
       <section className="popup popup-confirm">
         <form className="popup__container popup__container_confirm">
-          <button type="button" className="popup__close"></button>
+          <button type="button" className="popup__close"><img src={closeIcon} className="popup__close-image" alt="кнопка закрытия" /></button>/button>
           <h2 className="popup__title popup__title-confirm">Вы уверены?</h2>
           <div name="confirm" className="popup__form" novalidate>
             <button type="submit" className="popup__form-submit popup__form-submit_confirm">Да</button>
@@ -57,7 +59,7 @@ function App() {
 
       <section className="popup popup-update-avatar">
         <form className="popup__container popup__container_avatar">
-          <button type="button" className="popup__close"></button>
+          <button type="button" className="popup__close"><img src={closeIcon} className="popup__close-image" alt="кнопка закрытия" /></button>
           <h2 className="popup__title">Обновить аватар</h2>
           <div name="avatar" className="popup__form" novalidate>
             <section className="popup__section">
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,35 @@
 function Main() {
+
+    function handleEditAvatarClick() {
+        document.querySelector('.popup-update-avatar').classList.add('popup_opened');
+    }
+
+    function handleEditProfileClick() {
+        document.querySelector('.popup-profile').classList.add('popup_opened');
+    }
+
+
+    function handleAddPlaceClick() {
+        document.querySelector('.popup-add').classList.add('popup_opened');
+    }
     return (
         <main>
             <div className="content">
                 <section className="profile">
                     <div className="profile__avatar-block">
                         <img className="profile__avatar" src="#" alt="аватар" />
-                        <div className="profile__avatar-overlay"></div>
+                        <div onClick={handleEditAvatarClick} className="profile__avatar-overlay"></div>
                     </div>
                     <div className="profile__info">
                         <div className="profile__text">
                             <h1 className="profile__title">Жак-Ив Кусто</h1>
-                            <button className="profile__button-edit" type="button" aria-label="Редактировать профиль"></button>
+                            <button onClick={handleEditProfileClick} className="profile__button-edit" type="button" aria-label="Редактировать профиль"></button>
                         </div>
                         <p className="profile__subtitle">Исследователь океана</p>
 
                     </div>
 
-                    <button className="profile__button-add" aria-label="Добавить карточку" type="button"></button>
+                    <button onClick={handleAddPlaceClick} className="profile__button-add" aria-label="Добавить карточку" type="button"></button>
 
                 </section>
                 <section className="elements">
