0bbf3fea3687daf26ce5cebf420ab393284b80f5
Inna Dobrynina <a-ya-dobraya@yandex.ru>
2021-05-18 00:20:36 +0500

    Контекст текущего пользователя

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../index.css';
 import Header from './Header.js';
 import Main from './Main.js';
 import Footer from './Footer.js';
 import PopupWithForm from './PopupWithForm';
 import ImagePopup from './ImagePopup';
+import { CurrentUserContext } from '../contexts/CurrentUserContext';
+import api from '../utils/api';
 
 function App() {
 
@@ -14,6 +16,9 @@ function App() {
   const [isImagePopupOpen, setIsImagePopupOpen] = useState(false);
 
   const [selectedCard, setSelectedCard] = useState({ link: '', name: '', isOpen: false });
+
+  const [currentUser, setCurrentUser] = useState({});
+  const [cards, setCards] = useState([]);
 
   function handleEditAvatarClick() {
     setIsEditAvatarPopupOpen(true);
@@ -40,65 +45,118 @@ function App() {
     setSelectedCard({ link: '', name: '', isOpen: false })
   }
 
+  useEffect(() => {
+    api.getUserInfo()
+      .then((results) => {
+        setCurrentUser(
+          {
+            _id: results._id,
+            name: results.name,
+            about: results.about,
+            avatar: results.avatar
+          }
+        )
+      })
+      .catch((err) =>
+        console.log(`Ошибка: ${err}`));
+  }, [])
+
+  useEffect(() => {
+    api.getCards()
+      .then((results) => {
+        setCards(results)
+      })
+      .catch((err) =>
+        console.log(`Ошибка: ${err}`));
+  }, [])
+
+  function handleCardDelete(card) {
+    api.deleteCard(card._id).then(() => {
+      // Определяем, являемся ли мы владельцем текущей карточки
+      const isOwn = card.owner._id === currentUser._id;
+      setCards(isOwn);
+    })
+      .catch((err) =>
+        console.log(`Ошибка: ${err}`));
+  }
+
+  function handleCardLike(card) {
+    // Определяем, есть ли у карточки лайк, поставленный текущим пользователем
+    const isLiked = card.likes.some(i => i._id === currentUser._id);
+
+
+    api.putLike(card._id, !isLiked)
+      .then((newCard) => {
+        setCards((state) => state.map((c) => c._id === card._id ? newCard : c));
+      })
+      .catch((err) =>
+        console.log(`Ошибка: ${err}`));
+  }
+
   return (
-    <div className="root">
-      <Header />
-      <Main
-        onEditAvatar={handleEditAvatarClick}
-        onEditProfile={handleEditProfileClick}
-        onAddPlace={handleAddPlaceClick}
-        onCardClick={handleCardClick}
-      />
+    <CurrentUserContext.Provider value={currentUser}>
+      <div className="root">
+        <Header />
+        <Main
+          onEditAvatar={handleEditAvatarClick}
+          onEditProfile={handleEditProfileClick}
+          onAddPlace={handleAddPlaceClick}
+          onCardClick={handleCardClick}
+          cards={cards}
+          onCardDelete={handleCardDelete}
+          onCardLike={handleCardLike}
+        />
 
-      <Footer />
-      <PopupWithForm
-        title='Редактировать профиль'
-        name='profile'
-        buttonText='Сохранить'
-        isOpen={isEditProfilePopupOpen}
-        onClose={closeAllPopups}>
-        <input className="popup__input popup__input_name" type="text" id="title-name" name="name" placeholder="Имя" aria-label="Имя" required minLength={2} maxLength={40} />
-        <span className="popup__input-error popup__input-error_name" id="title-name-error"></span>
-        <input className="popup__input popup__input_about" type="text" id="about-name" name="about" placeholder="Род деятельности" aria-label="О себе" required minLength={2} maxLength={200} />
-        <span className="popup__input-error popup__input-error_about" id="about-name-error"></span>
-      </PopupWithForm>
+        <Footer />
+        <PopupWithForm
+          title='Редактировать профиль'
+          name='profile'
+          buttonText='Сохранить'
+          isOpen={isEditProfilePopupOpen}
+          onClose={closeAllPopups}>
+          <input className="popup__input popup__input_name" type="text" id="title-name" name="name" placeholder="Имя" aria-label="Имя" required minLength={2} maxLength={40} />
+          <span className="popup__input-error popup__input-error_name" id="title-name-error"></span>
+          <input className="popup__input popup__input_about" type="text" id="about-name" name="about" placeholder="Род деятельности" aria-label="О себе" required minLength={2} maxLength={200} />
+          <span className="popup__input-error popup__input-error_about" id="about-name-error"></span>
+        </PopupWithForm>
 
-      <PopupWithForm
-        title='Новое место'
-        name='add'
-        buttonText='Создать'
-        isOpen={isAddPlacePopupOpen}
-        onClose={closeAllPopups}>
-        <input className="popup__input popup__input_place" type="text" name="name" id="place-name" placeholder="Название" required minLength={2} maxLength={30} />
-        <span className="popup__input-error popup__input-error_place" id="place-name-error"></span>
-        <input className="popup__input popup__input_image" type="url" name="link" id="link-image" placeholder="Ссылка на картинку" required />
-        <span className="popup__input-error popup__input-error_image" id="link-image-error"></span>
-      </PopupWithForm>
+        <PopupWithForm
+          title='Новое место'
+          name='add'
+          buttonText='Создать'
+          isOpen={isAddPlacePopupOpen}
+          onClose={closeAllPopups}>
+          <input className="popup__input popup__input_place" type="text" name="name" id="place-name" placeholder="Название" required minLength={2} maxLength={30} />
+          <span className="popup__input-error popup__input-error_place" id="place-name-error"></span>
+          <input className="popup__input popup__input_image" type="url" name="link" id="link-image" placeholder="Ссылка на картинку" required />
+          <span className="popup__input-error popup__input-error_image" id="link-image-error"></span>
+        </PopupWithForm>
 
-      <PopupWithForm
-        title='Обновить аватар'
-        name='update-avatar'
-        buttonText='Обновить'
-        isOpen={isEditAvatarPopupOpen}
-        onClose={closeAllPopups}>
+        <PopupWithForm
+          title='Обновить аватар'
+          name='update-avatar'
+          buttonText='Обновить'
+          isOpen={isEditAvatarPopupOpen}
+          onClose={closeAllPopups}>
 
-        <input id="avatar-link" type="url" name="avatar" className="popup__input" placeholder="Ссылка на аватар" required aria-label="Ссылка на аватар" />
-        <span className="popup__input-error popup__input-error_avatar" id="avatar-link-error"></span>
+          <input id="avatar-link" type="url" name="avatar" className="popup__input" placeholder="Ссылка на аватар" required aria-label="Ссылка на аватар" />
+          <span className="popup__input-error popup__input-error_avatar" id="avatar-link-error"></span>
 
-      </PopupWithForm>
+        </PopupWithForm>
 
-      <PopupWithForm
-        title='Вы уверены?'
-        name='popup-confirm'
-        buttonText='Сохранить'>
-      </PopupWithForm>
+        <PopupWithForm
+          title='Вы уверены?'
+          name='popup-confirm'
+          buttonText='Сохранить'>
+        </PopupWithForm>
 
-      <ImagePopup
-        card={selectedCard}
-        isOpen={isImagePopupOpen}
-        onClose={closeAllPopups}
-      />
-    </div>
+        <ImagePopup
+          card={selectedCard}
+          isOpen={isImagePopupOpen}
+          onClose={closeAllPopups}
+        />
+      </div>
+    </CurrentUserContext.Provider>
   );
 }
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,58 +1,59 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import api from '../utils/api.js';
 import Card from './Card.js';
+import { CurrentUserContext } from '../contexts/CurrentUserContext'
 
 
-function Main({ onEditProfile, onAddPlace, onEditAvatar, onCardClick }) {
-    const [user, setUser] = useState();
-    const [cards, setCards] = useState([]);
-    const [userDescription, setUserDescription] = useState();
-    const [userAvatar, setUserAvatar] = useState();
+function Main(props) {
+    const currentUser = React.useContext(CurrentUserContext);
+
+    /* const [user, setUser] = useState(); */
+
+    /*  const [userDescription, setUserDescription] = useState();
+     const [userAvatar, setUserAvatar] = useState(); */
 
 
-    useEffect(() => {
-        api.getUserInfo()
-            .then(results => {
-                setUser(results.name);
-                setUserDescription(results.about);
-                setUserAvatar(results.avatar);
-            })
-            .catch(err =>
-                console.log(err));
-    }, []);
+    /*   useEffect(() => {
+          api.getUserInfo()
+              .then(results => {
+                  setUser(results.name);
+                  setUserDescription(results.about);
+                  setUserAvatar(results.avatar);
+              })
+              .catch(err =>
+                  console.log(err));
+      }, []); */
 
-    useEffect(() => {
-        api.getCards()
-            .then(results => {
-                setCards(results);
-            })
-            .catch(err =>
-                console.log(err));
-    }, []);
+
 
     return (
         <main>
             <div className="content">
                 <section className="profile">
                     <div className="profile__avatar-block">
-                        <div className="profile__avatar" alt="аватар" style={{ backgroundImage: `url(${userAvatar})` }} />
-                        <div onClick={onEditAvatar} className="profile__avatar-overlay"></div>
+                        <div className="profile__avatar" alt="аватар" style={{ backgroundImage: `url(${currentUser.avatar})` }} />
+                        <div onClick={props.onEditAvatar} className="profile__avatar-overlay"></div>
                     </div>
                     <div className="profile__info">
                         <div className="profile__text">
-                            <h1 className="profile__title">{user}</h1>
-                            <button onClick={onEditProfile} className="profile__button-edit" type="button" aria-label="Редактировать профиль"></button>
+                            <h1 className="profile__title">{currentUser.name}</h1>
+                            <button onClick={props.onEditProfile} className="profile__button-edit" type="button" aria-label="Редактировать профиль"></button>
                         </div>
-                        <p className="profile__subtitle">{userDescription}</p>
+                        <p className="profile__subtitle">{currentUser.about}</p>
 
                     </div>
 
-                    <button onClick={onAddPlace} className="profile__button-add" aria-label="Добавить карточку" type="button"></button>
+                    <button onClick={props.onAddPlace} className="profile__button-add" aria-label="Добавить карточку" type="button"></button>
 
                 </section>
                 <section className="elements">
-                    {cards.map(item => (
-                        <Card key={item._id} card={item} onCardClick={onCardClick} />
+                    {props.cards.map((card) => (
+                        <Card
+                            key={card._id}
+                            card={card}
+                            onCardClick={props.onCardClick}
+                            onCardDelete={props.handleCardDelete}
+                            onCardLike={props.handleCardLike} />
                     ))}
                 </section>
             </div>
